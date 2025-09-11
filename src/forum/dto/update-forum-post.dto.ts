49756ac7396461c8f71d2c60dd737172f6efedf6
jason221126 <jc6125398@gmail.com>
2025-09-11 22:20:36 +0800

    Delete src directory

--- src/forum/dto/update-forum-post.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateForumPostDto } from './create-forum-post.dto';
-
-export class UpdateForumPostDto extends PartialType(CreateForumPostDto) {}
